306fb6a846fe957f39a3ccecc31a933cc8391377
BethanyDrake <drake_beth@yahoo.com.au>
2017-11-19 11:53:21 +1100

    display text

--- app/src/main/java/sycorax/writecheck/MainActivity.java
+++ app/src/main/java/sycorax/writecheck/MainActivity.java
@@ -3,6 +3,7 @@ package sycorax.writecheck;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         canvasView = (CanvasView) findViewById(R.id.canvas);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+
+
+    TextView textView;
+    int i = 0;
+    public void nextCard(View v)
+    {
+        i++;
+        String t = "hi" + i;
+        textView.setText(t);
+
+    }
+
+    public void flipCard(View v)
+    {
+
     }
 
     public void clearCanvas(View v)
     {
         canvasView.clearCanvas();
+        nextCard(v);
     }
 }
